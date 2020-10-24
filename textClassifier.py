3a86fcaa69da9d71d6c75411670b421c20404ace
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-10-25 00:27:29 +0100

    Fix list mutation

--- textClassifier.py
+++ textClassifier.py
@@ -1,15 +1,18 @@
+import nltk
 import os
 import re
+
+from nltk.corpus import stopwords
 
 # load in data
 def load_data():
   """"""
   
   topics = ['business', 
-             'entertainment',
-             'politics', 
-             'sport', 
-             'tech']
+            'entertainment',
+            'politics', 
+            'sport', 
+            'tech']
   
   data_directory = os.path.join(os.getcwd(), 'data')
   documents_by_class = []
@@ -33,20 +36,26 @@ def load_data():
 def process_data(documents_by_class):
   """"""
   for topic_documents in documents_by_class:
+    # remove spaces
+    topic_documents[:] = [' '.join(document.split()) for document in topic_documents]
+    
     # remove punctuation ( !"£$%^&*()-_+=[]{};:'@#~\|,<.>/? ) and numbers
     # TODO: consider if there's an optimisation using a lambda function
-    topic_documents = [re.sub('[a-zA-Z]', ' ', document) for document in topic_documents]             
+    topic_documents[:] = [re.sub('[^a-zA-Z]', ' ', document) for document in topic_documents]             
         
     # consider removing single-character words 
     # (which might be the result of removing punctuation and numbers)
-    topic_documents = [' '.join([w for w in document.split() if len(w) > 1]) for document in topic_documents]
+    topic_documents[:] = [' '.join([w for w in document.split() if len(w) > 1]) for document in topic_documents]
   
     # lowercase all words
-    topic_documents = [document.lower() for document in topic_documents]
+    topic_documents[:] = [document.lower() for document in topic_documents]
   
     # tokenize
+    topic_documents[:] = [document.split() for document in topic_documents]
   
-    # remove stopwords
+    # remove stopwords (use nltk?)
+    topic_documents[:] = [[word for word in doc if word not in stopwords.words("english")] 
+                          for doc in topic_documents]
     
     # lemmatize the words
 
@@ -58,10 +67,22 @@ def process_data(documents_by_class):
 
 # evaluate the model
 
-if __name__ == "__main__":
-  documents_by_class = load_data()
-  documents_by_class = process_data(documents_by_class)
-  for topic_documents in documents_by_class:
-    print('New topic\n')
-    for document in topic_documents:
-      print(document[:50])
+if __name__ == "__main__":     
+  documents_by_class = load_data()    
+  # topic_1 = documents_by_class[0]
+  # topic_1[:] = [' '.join(document.split()) for document in topic_1]
+  # print(topic_1[0], '\n') 
+  # print(documents_by_class[0][0])
+  #print(single_doc)
+  # single_doc = ' '.join(single_doc.split())
+  #print(single_doc)
+  # single_doc = re.sub('[^a-zA-Z]', ' ', single_doc)
+  # print(single_doc)
+
+  #documents_by_class[:] = process_data(documents_by_class)
+  # for document in documents_by_class[0]:
+  #   print(document[:50])
+  # for topic_documents in documents_by_class:
+  #   print('New topic\n')
+  #   for document in topic_documents:
+  #     print(document[:50], '\n')
