7b22f428683af5a27d267befe8bd2ad277f61761
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-10-29 20:16:57 +0000

    Finish initial text preprocessing code

--- webapp/app.py
+++ webapp/app.py
@@ -1,5 +1,7 @@
 import pickle
+import re
 
+import emoji
 import flask
 import numpy as np
 import pandas as pd
@@ -9,78 +11,7 @@ import yaml
 
 import text_classifier
 
-with open('model/classifier.pkl', 'rb') as f:
-  classifier = pickle.load(f)
-
 app = flask.Flask(__name__, template_folder='templates')
-
-@app.route('/', methods=['GET', 'POST'])
-def main():
-  if flask.request.method == 'GET':
-    return flask.render_template('main.html')
-
-  if flask.request.method == 'POST':
-    topic = flask.request.form['topic']
-    endpoint_url = f'https://api.twitter.com/2/tweets/search/recent?query={topic}&tweet.fields=created_at,lang&max_results=100'
-    data = process_yaml()
-    bearer_token = create_bearer_token(data)
-    response_json = twitter_auth_and_connect(bearer_token, endpoint_url)
-    
-    # Test what happens when a topic that has no tweets (for the specified time period) is used in the request.
-    # Update the code below accordingly.
-    
-    # Filter for English tweets
-    # Q: are you sure you want to modify the original response? A: TBC    
-    if 'data' in response_json: # Q: Is this necessary? A: TBC
-      response_json['data'] = ([tweet for tweet in response_json['data'] 
-                                if tweet['lang'] == "en" 
-                                or tweet['lang'] == "en-gb"])
-
-    # TODO: Consider wrapping the necessary code in a try, 
-    # in case the data that is returned is not as expected and you get an exception
-    # Replace retweets that end in an ellipsis with the full tweet
-    # ============================================================
-    # Q: Is this robust to potential changes to the unicode? A: TBC
-    # Q: Is it possible that there are different unicode or other code values for this symbol? A: TBC
-    ellipsis_unicode = '\u2026'     
-    retweet_abbreviation = 'RT'
-    retweeted_tweets = {}
-    # For each tweet (which I'm almost sure will be a RT) in data that ends in an ellipsis
-    for tweet in response_json['data']:                  
-      ## if the tweet text ends in an ellipsis and the string starts with RT
-      ## (I haven't come across any cases where the former is true and the latter isn't; so the latter might not be necessary)
-      ## (I'll leave it in just in case)
-      if (tweet['text'].endswith(ellipsis_unicode) 
-          and tweet['text'].startswith(retweet_abbreviation)):
-        ## Replace the tweet with the full text of the tweet    
-        # 1. get ID of referenced tweet
-        for tweet_reference in tweet['referenced_tweets']:
-          if tweet_reference['type'] == 'retweeted':
-            retweeted_tweet_id = tweet_reference['id']
-            break                      
-        # 2. get full tweet
-        if retweeted_tweet_id in retweeted_tweets:
-          full_tweet = retweeted_tweets[retweeted_tweet_id]
-        else:
-          for referenced_tweet in response_json['includes']['tweets']:
-            if referenced_tweet['id'] == retweeted_tweet_id:
-              full_tweet = referenced_tweet['text']
-              retweeted_tweets[retweeted_tweet_id] = full_tweet
-              break
-        
-        tweet['full_text'] = full_tweet
-
-    # For each tweet
-    ## Consider removing \n, \" and &amp; (and similar &xxx character combos); might not be a problem if ascii set to false (maybe not for the &xxx, though)
-    ## Remove URLs (probably regex)
-    ## Remove emojis might not be a problem if ascii set to false
-    ## Remove user mentions (the @ symbol and the username)
-    ## remove repeated characters (maybe use nltk word_tokenize) if they won't be removed in the classifier
-    ## Consider removing hashtags (just the hash symbol) if they won't be removed in the classifier
-    ## Maybe consider splitting up multi-word hashtags
-
-    # TODO: replace 'sentiment' with what it should be
-    return flask.render_template('main.html', result=sentiment)
 
 def process_yaml():
   with open("config.yaml") as file:
@@ -89,10 +20,96 @@ def process_yaml():
 def create_bearer_token(data):
   return data["search_tweets_api"]["bearer_token"]
 
-def twitter_auth_and_connect(bearer_token, url):
+def make_request(bearer_token, url):
   headers = {"Authorization": "Bearer {}".format(bearer_token)}
   response = requests.request("GET", url, headers=headers)
   return response.json()
+
+def get_tweet_data(topic, max_results):  
+  base_url = 'https://api.twitter.com/2/tweets/search/recent'  
+  parameters = f'query={topic}&tweet.fields=created_at,lang&max_results={max_results}&expansions=referenced_tweets.id'
+  endpoint_url = f'{base_url}?{parameters}'    
+  bearer_token = create_bearer_token(process_yaml())
+  return make_request(bearer_token, endpoint_url)
+
+def remove_html_character_entities(tweet):
+  tweet['text'] = re.sub(r'&[a-zA-Z]+;', '', tweet['text'])
+
+def remove_urls(tweet):
+  tweet['text'] = re.sub(r'www\.\S+|https?://\S+', '', tweet['text'])
+
+def remove_emoji(tweet):
+  tweet['text'] = emoji.get_emoji_regexp().sub(u'', tweet['text'])
+
+def remove_at_mentions(tweet):
+  tweet['text'] = re.sub(r'@\S+', '', tweet['text'])
+
+def remove_retweet_abbreviation(tweet):
+  tweet['text'] = re.sub(r'', '', tweet['text'])
+
+def process_tweets(response_json):
+  # TODO: Consider wrapping the necessary code in a try, 
+  # in case the data that is returned is not as expected and you get an exception    
+    
+  ellipsis_unicode = '\u2026'     
+  retweet_abbreviation = 'RT'
+  retweeted_tweets = {}
+
+  for tweet in response_json['data']:                        
+    remove_html_character_entities(tweet)
+    remove_urls(tweet)
+    remove_emoji(tweet)
+    remove_at_mentions(tweet)
+    if (tweet['text'].endswith(ellipsis_unicode) 
+        and tweet['text'].startswith(retweet_abbreviation)):        
+      
+      for tweet_reference in tweet['referenced_tweets']:
+        if tweet_reference['type'] == 'retweeted':
+          retweeted_tweet_id = tweet_reference['id']
+          break                              
+      if retweeted_tweet_id in retweeted_tweets:
+        full_tweet = retweeted_tweets[retweeted_tweet_id]
+      else:
+        for referenced_tweet in response_json['includes']['tweets']:
+          if referenced_tweet['id'] == retweeted_tweet_id:
+            full_tweet = referenced_tweet['text']
+            retweeted_tweets[retweeted_tweet_id] = full_tweet
+            break        
+      tweet['text'] = full_tweet      
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+  if flask.request.method == 'GET':
+    return flask.render_template('main.html')
+
+  if flask.request.method == 'POST':    
+    max_results = 10 # TODO: change to 100 for live app        
+    response_json = get_tweet_data(flask.request.form['topic'], max_results)
+    
+    # Test what happens when a topic that has no tweets (for the specified time period) is used in the request.
+    # and update the code below accordingly.
+    
+    # Q: are you sure you want to modify the original response? A: TBC    
+    if 'data' in response_json: # Q: Is this necessary? A: TBC
+      response_json['data'] = ([tweet for tweet in response_json['data'] 
+                                if tweet['lang'] == "en" 
+                                or tweet['lang'] == "en-gb"])            
+
+    process_tweets(response_json) 
+
+    tweets = []
+    for tweet in response_json['data'] :
+      tweets.append(tweet['text'])
+
+    
+
+    with open('model/classifier.pkl', 'rb') as f:
+      classifier = pickle.load(f)
+
+    predictions = classifier.predict(tweets.to_numpy())
+
+    # TODO: replace 'sentiment' with what it should be
+    return flask.render_template('main.html', result=sentiment)
 
 if __name__ == '__main__':
   app.run()
--- webapp/playground.py
+++ webapp/playground.py
@@ -1,57 +1,64 @@
 import copy
 import json
+import re
 
+import emoji
+import nltk
 import requests
 import twitter
 import yaml
-
-def create_bearer_token(data):
-  return data["search_tweets_api"]["bearer_token"]
 
 def process_yaml():
   with open("config.yaml") as file:
     return yaml.safe_load(file)
 
-def twitter_auth_and_connect(bearer_token, url):
+def create_bearer_token(data):
+  return data["search_tweets_api"]["bearer_token"]
+
+def make_request(bearer_token, url):
   headers = {"Authorization": "Bearer {}".format(bearer_token)}
   response = requests.request("GET", url, headers=headers)
   return response.json()
 
-def main():
-  endpoint_url = 'https://api.twitter.com/2/tweets/search/recent?query=covid&tweet.fields=created_at,lang,entities&expansions=referenced_tweets.id'
-  data = process_yaml()
-  bearer_token = create_bearer_token(data)
-  response_json = twitter_auth_and_connect(bearer_token, endpoint_url)        
+def get_tweets(topic, max_results):  
+  base_url = 'https://api.twitter.com/2/tweets/search/recent'  
+  parameters = f'query={topic}&tweet.fields=created_at,lang&max_results={max_results}&expansions=referenced_tweets.id'
+  endpoint_url = f'{base_url}?{parameters}'    
+  bearer_token = create_bearer_token(process_yaml())
+  return make_request(bearer_token, endpoint_url)
 
-  if 'data' in response_json: 
-    response_json['data'] = ([tweet for tweet in response_json['data'] 
-                              if tweet['lang'] == "en" 
-                              or tweet['lang'] == "en-gb"])
-      
-  id = '10'
-  
-  with open(f'response-{id}-ensure-ascii-false.json', 'w', encoding='utf-8') as f:
-    json.dump(response_json, f, ensure_ascii=False, indent=2)  
-  with open(f'response-{id}.json', 'w', encoding='utf-8') as f:
-    json.dump(response_json, f, indent=2)  
-  
+def remove_html_character_entities(tweet):
+  tweet['text'] = re.sub(r'&[a-zA-Z]+;', '', tweet['text'])
+
+def remove_urls(tweet):
+  tweet['text'] = re.sub(r'www\.\S+|https?://\S+', '', tweet['text'])
+
+def remove_emoji(tweet):
+  tweet['text'] = emoji.get_emoji_regexp().sub(u'', tweet['text'])
+
+def remove_at_mentions(tweet):
+  tweet['text'] = re.sub(r'@\S+', '', tweet['text'])
+
+def process_tweets(response_json):
+  # TODO: Consider wrapping the necessary code in a try, 
+  # in case the data that is returned is not as expected and you get an exception    
+    
   ellipsis_unicode = '\u2026'     
   retweet_abbreviation = 'RT'
   retweeted_tweets = {}
-  # For each tweet (which I'm almost sure will be a RT) in data that ends in an ellipsis
-  for tweet in response_json['data']:                  
-    ## if the tweet text ends in an ellipsis and the string starts with RT
-    ## (I haven't come across any cases where the former is true and the latter isn't; so the latter might not be necessary)
-    ## (I'll leave it in just in case)
+
+  for tweet in response_json['data']:                        
+    remove_html_character_entities(tweet)
+    remove_urls(tweet)
+    remove_emoji(tweet)
+    remove_at_mentions(tweet)
     if (tweet['text'].endswith(ellipsis_unicode) 
-        and tweet['text'].startswith(retweet_abbreviation)):
-      ## Replace the tweet with the full text of the tweet    
-      # 1. get ID of referenced tweet
+        and tweet['text'].startswith(retweet_abbreviation)):        
+      
       for tweet_reference in tweet['referenced_tweets']:
         if tweet_reference['type'] == 'retweeted':
           retweeted_tweet_id = tweet_reference['id']
-          break                      
-      # 2. get full tweet
+          break                              
       if retweeted_tweet_id in retweeted_tweets:
         full_tweet = retweeted_tweets[retweeted_tweet_id]
       else:
@@ -59,12 +66,44 @@ def main():
           if referenced_tweet['id'] == retweeted_tweet_id:
             full_tweet = referenced_tweet['text']
             retweeted_tweets[retweeted_tweet_id] = full_tweet
-            break
-      
+            break        
       tweet['full_text'] = full_tweet
+      tweet['text'] = tweet['text'][2:]
 
-  with open(f'response-{id}-retweets-replaced.json', 'w', encoding='utf-8') as f:
-    json.dump(response_json, f, indent=2)
+def main():                              
+  text = 'a a a the the the the other words'
+  freq_dist = nltk.FreqDist(text)
+  print(freq_dist.keys())
+  pass
+  # with open('response-11.json', 'r') as f:
+  #   response_json = json.load(f)
+  #   for tweet in response_json['data']:
+  #     tweet['text'] = re.sub(r'@\S+', '', tweet['text'])
+  #   with open('response-11-no-at-mentions.json', 'w', encoding='utf-8') as f2:
+  #     json.dump(response_json, f2, indent=2)
+
+  
+  #pass
+  # max_results = 10 # CAREFUL!
+  # topic = 'covid'
+  # response_json = get_tweets(topic, max_results)    
+    
+  # if 'data' in response_json: 
+  #   response_json['data'] = ([tweet for tweet in response_json['data'] 
+  #                             if tweet['lang'] == "en" 
+  #                             or tweet['lang'] == "en-gb"])              
+  
+  # id = '11'
+  
+  # with open(f'response-{id}-ensure-ascii-false.json', 'w', encoding='utf-8') as f:
+  #   json.dump(response_json, f, ensure_ascii=False, indent=2)  
+  # with open(f'response-{id}.json', 'w', encoding='utf-8') as f:
+  #   json.dump(response_json, f, indent=2)  
+  
+  # process_tweets(response_json)            
+
+  # with open(f'response-{id}-tweets-processed.json', 'w', encoding='utf-8') as f:
+  #   json.dump(response_json, f, indent=2)
 
 if __name__ == "__main__":
   main()
