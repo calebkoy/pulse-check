5cdd5228165d5d49562d689e31648037f55ca803
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-10-29 13:26:32 +0000

    Experiment with tweet processing

--- webapp/app.py
+++ webapp/app.py
@@ -6,7 +6,6 @@ import pandas as pd
 import requests
 import twitter
 import yaml
-# from imdb import IMDb
 
 import text_classifier
 
@@ -21,25 +20,43 @@ def main():
     return flask.render_template('main.html')
 
   if flask.request.method == 'POST':
-    # TODO: remove; only for testing
-    # review = flask.request.form['movie_review']
-    # classifier_input = np.array(review).reshape((1, 1))
-    # prediction = classifier.predict(classifier_input)
-    # sentiment = 'positive' if prediction[0] == 1 else 'negative'
+    topic = flask.request.form['topic']
+    endpoint_url = f'https://api.twitter.com/2/tweets/search/recent?query={topic}&tweet.fields=created_at,lang&max_results=100'
+    data = process_yaml()
+    bearer_token = create_bearer_token(data)
+    response = twitter_auth_and_connect(bearer_token, endpoint_url)
     
+    # Filter for English tweets
+    # Q: are you sure you want to modify the original response? A: TBC
+    # TODO: check that the response has a 'data' key first    
+    response['data'] = [obj for obj in response['data'] if obj['lang'] == "en" or obj['lang'] == "en-gb"]
 
-    # TODO: Get a set of reviews for this movie
-    movie_name = flask.request.form['movie']
-    imdb_instance = IMDb() 
-    movie_matches = imdb_instance.search_movie(movie_name)
-    # reviews_df = TBC
+    # For each tweet (which I'm almost sure will be a RT) in data that ends in an ellipsis
+    ## Replace the tweet with the full text of the tweet
 
-    # TODO: Predict the sentiment of these reviews
-    # predictions = classifier.predict(reviews_df.to_numpy())
-    # total_positive_reviews = len(predictions[predictions==1])
-    # total_negative_reviews = len(predictions[predictions==0])
+    # For each tweet
+    ## Consider removing \n, \" and &amp; (and similar &xxx character combos); might not be a problem if ascii set to false (maybe not for the &xxx, though)
+    ## Remove URLs (probably regex)
+    ## Remove emojis might not be a problem if ascii set to false
+    ## Remove user mentions (the @ symbol and the username)
+    ## remove repeated characters (maybe use nltk word_tokenize) if they won't be removed in the classifier
+    ## Consider removing hashtags (just the hash symbol) if they won't be removed in the classifier
+    ## Maybe consider splitting up multi-word hashtags
 
+    # TODO: replace 'sentiment' with what it should be
     return flask.render_template('main.html', result=sentiment)
+
+def process_yaml():
+  with open("config.yaml") as file:
+    return yaml.safe_load(file)
+
+def create_bearer_token(data):
+  return data["search_tweets_api"]["bearer_token"]
+
+def twitter_auth_and_connect(bearer_token, url):
+  headers = {"Authorization": "Bearer {}".format(bearer_token)}
+  response = requests.request("GET", url, headers=headers)
+  return response.json()
 
 if __name__ == '__main__':
   app.run()
--- webapp/temp_script.py
+++ webapp/temp_script.py
@@ -1,3 +1,6 @@
+import copy
+import json
+
 import requests
 import twitter
 import yaml
@@ -15,11 +18,22 @@ def twitter_auth_and_connect(bearer_token, url):
   return response.json()
 
 def main():
-  endpoint_url = 'https://api.twitter.com/2/tweets/search/recent?max_results=100&query=covid'
+  endpoint_url = 'https://api.twitter.com/2/tweets/search/recent?query=covid&tweet.fields=created_at,lang,entities&expansions=referenced_tweets.id'
   data = process_yaml()
   bearer_token = create_bearer_token(data)
-  res_json = twitter_auth_and_connect(bearer_token, endpoint_url)
-  print(res_json)
+  res_json = twitter_auth_and_connect(bearer_token, endpoint_url)  
+  res_json_copy = copy.deepcopy(res_json)  
+  res_json_copy['data'] = [obj for obj in res_json_copy['data'] if obj['lang'] == "en" or obj['lang'] == "en-gb"]  
+
+  #print([object for object in res_json['data'] if object['lang'] == "en" or object['lang'] == "en-gb"], '\n')  
+  #print(res_json)
+  # TODO: DUMP THE NEXT BATCH USING PRETTY JSON AND WITHOUT!!    
+  with open('temp-tweets-7-ensure-ascii-true-utf-8.json', 'w', encoding='utf-8') as f:
+    json.dump(res_json, f, indent=2)
+  with open('temp-tweets-7-ensure-ascii-false-utf-8.json', 'w', encoding='utf-8') as f:
+    json.dump(res_json, f, ensure_ascii=False, indent=2)
+  with open('temp-tweets-7-ensure-ascii-false-utf-8-only-en.json', 'w', encoding='utf-8') as f:
+    json.dump(res_json_copy, f, ensure_ascii=False, indent=2)
 
 if __name__ == "__main__":
   main()
