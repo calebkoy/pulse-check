272303f58fbe78a43e8d7ec75e5b8515d3b51e7b
Caleb Owusu-Yianoma <caleb_owusuyianoma@hotmail.co.uk>
2020-11-05 11:13:07 +0000

    Reorganise

--- pulse_check/test_app.py
+++ pulse_check/test_app.py
@@ -1,7 +1,5 @@
 import unittest
 
-print('__file__={0:<35} | __name__={1:<20} | __package__={2:<20}'.format(__file__,__name__,str(__package__)))
-
 from app import app
 
 class AppTests(unittest.TestCase):
